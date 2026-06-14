39eb43f0e3b32a8ee6e645337e0f8a922b9841b3
matt <matt@manjaro.localdomain>
2026-06-14 19:28:21 -0400

    Added a second way of MCA with only 2 factor levels for each variable

--- MCATesting.R
+++ MCATesting.R
@@ -10,9 +10,22 @@ data <- read.csv('data.csv') %>% select(!X)
 MCAexclude = c("EDUC","CLASS","WRKSTAT","MARITAL","AGE","CHILDS","SEX","RELIG", "INTRUST","TRPPL")
 
 #create dataset for MCA
-MCAdata <- data %>% select(!all_of(MCAexclude)) %>% lapply(factor) %>% data.frame()
+MCAdata1 <- data %>% select(!all_of(MCAexclude)) %>% lapply(factor) %>% data.frame()
 
-mca1 <- MCA(MCAdata)
+mca1 <- MCA(MCAdata1)
 
 #This will open a web browser interface
-Factoshiny(MCAdata)
+#Factoshiny(MCAdata1)
+
+#Going to try encoding the variable differently - instead of creating a boolean variable for each level of each variable, this will
+##create a boolean that signifies if the variable is greater than the median value.
+
+#I think this works? idk my brain kind of hurts with this code
+MCAdata2 <- data %>% select(!all_of(MCAexclude)) %>% mutate(across(everything(),function(x){
+  as.integer(x>median(data[[cur_column()]]))
+    }
+    )
+  ) %>% lapply(factor) %>% data.frame()
+
+
+mca2 <- MCA(MCAdata2)
